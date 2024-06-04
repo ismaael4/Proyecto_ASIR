dfa68ac4e4ab6ea7fd72d551b76b72abd252cdea
ismaael4 <124279743+ismaael4@users.noreply.github.com>
2024-06-04 19:48:44 +0200

    Versión 2.0
    Añadidas varias funciones además del documento MD

---
+++ netdata6.0/instalation.md
@@ -0,0 +1,320 @@
+﻿<details>
+<summary>Menú</summary>
+
+[Requisitos](#requisitos)  
+[Cambiar Hostname](#cambiar-hostname)  
+[Pruebas](#pruebas)  
+[Script para Cambiar Localhost por la IP (CORS)](#script-para-cambiar-localhost-por-la-ip-cors)  
+[Cambiar Nombre de Host y Reclamar Netdata](#cambiar-nombre-de-host-y-reclamar-netdata)  
+[Descargar Archivos desde Servidor HFS](#descargar-archivos-desde-servidor-hfs)
+
+</details>
+
+[![Texto alternativo](./img/netdata.png)](https://app.netdata.cloud/spaces/ismaprueb01-space/rooms/all-nodes/home?utm_source=website&utm_content=top_navigation_sign_in#metrics_correlation=false&after=-900&before=0&utc=Europe%2FMadrid&offset=%2B2&timezoneName=Brussels%2C%20Copenhagen%2C%20Madrid%2C%20Paris&modal=&modalTab=&modalParams=&selectedIntegrationCategory=deploy.operating-systems&force_play=false)
+
+
+
+# NETDATA DOCS
+## Requisitos
+```bash
+wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/ kickstart.sh && sh /tmp/netdata-kickstart.sh # Instalar NETDATA
+apt install apache2 #Servidor para el DASHBOARD
+apt install npm #Para instalar paquetes
+npm install chartjs-adapter-date-fns  # Para las gráficas
+yarn add chartjs-adapter-date-fns  # igual q el de arriba
+wget -O /etc/netdata/netdata.conf http://localhost:19999/netdata.conf  # FICHERO DE CONFIGURACION
+```
+## Prometheus
+```bash
+cd /etc/netdata 2>/dev/null || cd /opt/netdata/etc/netdata
+sudo ./edit-config exporting.conf
+```
+
+## Cambiar Hostname
+sudo nano /etc/hosts
+
+sudo nano /etc/hostname
+## PRUEBAS
+### Disco + 1G
+```bash
+fallocate -l 1G fichero12.txt
+```
+### Stress CPU
+```bash
+stress --cpu 100 -t 10
+```
+## SCRIPT PARA CAMBIAR LOCALHOST POR LA IP (CORS)
+```bash
+#!/bin/bash
+
+# Función para modificar una palabra en un archivo
+modificar_palabra_en_archivo() {
+    # Solicitar al usuario el nombre del archivo
+    read -p "Por favor, ingresa el nombre del archivo que deseas modificar: " nombre_archivo
+
+    # Verificar si el archivo existe
+    if [ -f "$nombre_archivo" ]; then
+        # Solicitar al usuario la palabra que desea modificar
+        read -p "Ingresa la palabra que deseas modificar en el archivo: " palabra_original
+
+        # Solicitar al usuario la palabra por la que desea reemplazar
+        read -p "Ingresa la nueva palabra por la que deseas reemplazarla: " palabra_nueva
+
+        # Realizar el reemplazo en el archivo
+        sed -i "s/$palabra_original/$palabra_nueva/g" "$nombre_archivo"
+
+        echo "La palabra '$palabra_original' ha sido modificada por '$palabra_nueva' en el archivo '$nombre_archivo'."
+    else
+        echo "El archivo '$nombre_archivo' no existe."
+    fi
+}
+
+# Llamar a la función principal
+modificar_palabra_en_archivo
+```
+## Cambiar Nombre de Host y Reclamar Netdata
+Este script cambia namehost ademas de cambiar de rooms o instalar netdata
+
+```bash
+#!/bin/bash
+
+# Parar el servicio de Netdata si está en ejecución
+sudo systemctl stop netdata 2>/dev/null
+
+# Eliminar el archivo que contiene el ID del agente de Netdata si existe
+sudo rm /var/lib/netdata/registry/netdata.public.unique.id 2>/dev/null
+
+# Solicitar al usuario si desea cambiar el nombre de host
+read -p "¿Desea cambiar el nombre de host? (s/n): " change_hostname
+if [ "$change_hostname" == "s" ]; then
+    # Solicitar al usuario un nuevo nombre de host
+    read -p "Ingrese el nuevo nombre de host: " new_hostname
+
+    # Cambiar el nombre de host en /etc/hostname
+    echo "$new_hostname" | sudo tee /etc/hostname >/dev/null
+
+    # Cambiar el nombre de host en /etc/hosts
+    sudo sed -i "s/^\(127.0.1.1\s\+\).*$/\1$new_hostname/" /etc/hosts
+
+    echo "El nombre de host se ha cambiado a '$new_hostname'."
+else
+    echo "No se realizarán cambios en el nombre de host."
+fi
+
+# Solicitar al usuario que ingrese el comando completo para instalar y reclamar Netdata
+echo "Ingrese el comando completo para instalar y reclamar Netdata (asegúrate de incluir el token):"
+read -p "> " install_command
+
+# Ejecutar el comando proporcionado por el usuario
+eval "$install_command"
+
+# Iniciar el servicio de Netdata si no estaba en ejecución
+sudo systemctl start netdata 2>/dev/null
+```
+## Descargar Archivos desde Servidor HFS
+Descarga los archivos de la ip que le pongamos ademas de añadirlos directamente en la carpeta /var de apache.
+__Tener apache instalado antes.__
+
+
+```bash
+#!/bin/bash
+
+# Solicitar la dirección IP del servidor HFS
+read -p "Ingrese la dirección IP del servidor HFS: " ip_address
+
+# Solicitar el nombre del archivo a descargar
+read -p "Ingrese el nombre del archivo a descargar (incluyendo la extensión .zip): " file_name
+
+# Descargar el archivo desde el servidor HFS
+wget "http://$ip_address/$file_name"
+
+# Verificar si la descarga fue exitosa
+if [ $? -eq 0 ]; then
+    echo "El archivo $file_name se ha descargado correctamente."
+else
+    echo "Error: No se pudo descargar el archivo $file_name desde el servidor HFS."
+    exit 1
+fi
+
+# Descomprimir el archivo .zip
+unzip -q "$file_name"
+
+# Verificar si la descompresión fue exitosa
+if [ $? -eq 0 ]; then
+    echo "El archivo $file_name se ha descomprimido correctamente."
+else
+    echo "Error: No se pudo descomprimir el archivo $file_name."
+    exit 1
+fi
+
+# Copiar los archivos descomprimidos a /var/www/html
+sudo cp -r netdata5.0/* /var/www/html/
+
+# Verificar si la copia fue exitosa
+if [ $? -eq 0 ]; then
+    echo "Los archivos se han copiado correctamente a /var/www/html."
+else
+    echo "Error: No se pudieron copiar los archivos a /var/www/html."
+    exit 1
+fi
+
+echo "El proceso ha finalizado correctamente."
+```
+## ALL SCRIPT
+```bash
+#!/bin/bash
+
+#!/bin/bash
+
+# Función para modificar la palabra "localhost" en el archivo /var/www/html/script.js por la dirección IP de la máquina
+modificar_palabra_en_archivo() {
+    local nombre_archivo="/var/www/html/script.js"
+
+    # Verificar si el archivo existe
+    if [ -f "$nombre_archivo" ]; then
+        # Obtener la dirección IP de la máquina
+        local ip_address=$(hostname -I | awk '{print $1}')
+
+        # Reemplazar "localhost" por la dirección IP en el archivo
+        sed -i "s/localhost/$ip_address/g" "$nombre_archivo"
+
+        echo "Se ha modificado 'localhost' por '$ip_address' en el archivo '$nombre_archivo'."
+    else
+        echo "El archivo '$nombre_archivo' no existe."
+    fi
+}
+
+# Resto del script omitido por brevedad
+
+
+# Función para detener y reclamar Netdata
+detener_y_reclamar_netdata() {
+    # Parar el servicio de Netdata si está en ejecución
+    sudo systemctl stop netdata 2>/dev/null
+
+    # Eliminar el archivo que contiene el ID del agente de Netdata si existe
+    sudo rm /var/lib/netdata/registry/netdata.public.unique.id 2>/dev/null
+
+    # Solicitar al usuario si desea cambiar el nombre de host
+    read -p "¿Desea cambiar el nombre de host? (s/n): " change_hostname
+    if [ "$change_hostname" == "s" ]; then
+        # Solicitar al usuario un nuevo nombre de host
+        read -p "Ingrese el nuevo nombre de host: " new_hostname
+
+        # Cambiar el nombre de host en /etc/hostname
+        echo "$new_hostname" | sudo tee /etc/hostname >/dev/null
+
+        # Cambiar el nombre de host en /etc/hosts
+        sudo sed -i "s/^\(127.0.1.1\s\+\).*$/\1$new_hostname/" /etc/hosts
+
+        echo "El nombre de host se ha cambiado a '$new_hostname'."
+    else
+        echo "No se realizarán cambios en el nombre de host."
+    fi
+
+    # Solicitar al usuario que ingrese el comando completo para instalar y reclamar Netdata
+    echo "Ingrese el comando completo para instalar y reclamar Netdata (asegúrate de incluir el token):"
+    read -p "> " install_command
+
+    # Ejecutar el comando proporcionado por el usuario
+    eval "$install_command"
+
+    # Iniciar el servicio de Netdata si no estaba en ejecución
+    sudo systemctl start netdata 2>/dev/null
+}
+
+# Instalar Apache2
+instalar_apache2() {
+    sudo apt install apache2
+}
+
+# Instalar npm
+instalar_npm() {
+    sudo apt install npm
+}
+
+# Instalar paquete npm
+instalar_paquete_npm() {
+    npm install chartjs-adapter-date-fns
+}
+
+# Descargar el archivo de configuración de Netdata
+descargar_configuracion_netdata() {
+    sudo wget -O /etc/netdata/netdata.conf http://localhost:19999/netdata.conf
+}
+
+# Función para descargar archivos desde el servidor HFS
+download_and_copy() {
+    # Solicitar la dirección IP del servidor HFS
+    read -p "Ingrese la dirección IP del servidor HFS: " ip_address
+
+    # Solicitar el nombre del archivo a descargar
+    read -p "Ingrese el nombre del archivo a descargar (incluyendo la extensión .zip): " file_name
+
+    # Descargar el archivo desde el servidor HFS
+    wget "http://$ip_address/$file_name"
+
+    # Verificar si la descarga fue exitosa
+    if [ $? -eq 0 ]; then
+        echo "El archivo $file_name se ha descargado correctamente."
+    else
+        echo "Error: No se pudo descargar el archivo $file_name desde el servidor HFS."
+        exit 1
+    fi
+
+    # Descomprimir el archivo .zip
+    unzip -q "$file_name"
+
+    # Obtener el nombre de la carpeta descomprimida
+    folder_name=$(basename "$file_name" .zip)
+
+    # Eliminar contenido existente de la carpeta de Apache
+    sudo rm -rf /var/www/html/*
+
+    # Copiar los archivos descomprimidos a /var/www/html
+    sudo cp -r "$folder_name"/* /var/www/html/
+
+    # Verificar si la copia fue exitosa
+    if [ $? -eq 0 ]; then
+        echo "Los archivos se han copiado correctamente a /var/www/html."
+    else
+        echo "Error: No se pudieron copiar los archivos a /var/www/html."
+        exit 1
+    fi
+
+    echo "El proceso ha finalizado correctamente."
+}
+
+# Llamar a la función principal
+descargar_y_copiar_a_carpeta_apache
+
+# Menú principal
+while :
+do
+    clear
+    echo "Seleccione una opción:"
+    echo "1. Modificar palabra en un archivo"
+    echo "2. Detener y reclamar Netdata"
+    echo "3. Instalar Apache2"
+    echo "4. Instalar npm"
+    echo "5. Instalar Charts.js"
+    echo "6. Descargar archivo de configuración de Netdata"
+    echo "7. Descargar y Copiar archivos a la carpeta de Apache"
+    echo "8. Salir"
+    read -p "Opción: " opcion
+
+    case $opcion in
+        1) modificar_palabra_en_archivo ;;
+        2) detener_y_reclamar_netdata ;;
+        3) instalar_apache2 ;;
+        4) instalar_npm ;;
+        5) instalar_paquete_npm ;;
+        6) descargar_configuracion_netdata ;;
+        7) download_and_copy ;;
+        8) exit ;;
+        *) echo "Opción inválida"; read -p "Presiona Enter para continuar..." ;;
+    esac
+done
+
+```
+
---
+++ netdata6.0/script.js
@@ -0,0 +1,490 @@
+// Crear el gráfico de donut para la CPU
+var cpuChart = new Chart(document.getElementById('cpuChart').getContext('2d'), {
+    type: 'doughnut',
+    data: {
+        labels: ['Carga de CPU (%)', ''],
+        datasets: [{
+            data: [0, 100],
+            backgroundColor: ['#007bff', '#f0f0f0'] // Cambiar los colores aquí
+        }]
+    },
+    options: {
+        aspectRatio: 1,
+        cutoutPercentage: 50,
+        plugins: {
+            legend: {
+                display: false
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        return 'Carga de CPU: ' + context.parsed + '%';
+                    }
+                }
+            }
+        }
+    }
+});
+
+// Función para actualizar el gráfico de donut de la CPU
+function updateCPULoadChart(cpuLoad) {
+    // Formatear la carga de la CPU para mostrar solo dos decimales
+    const formattedCpuLoad = cpuLoad.toFixed(0);
+
+    // Actualizar el texto de la carga de la CPU
+    const cpuLoadText = formattedCpuLoad + '%';
+    document.querySelector('.cpu-load').textContent = cpuLoadText;
+
+    // Actualizar los datos del gráfico de donut de la CPU
+    cpuChart.data.datasets[0].data[0] = formattedCpuLoad;
+    cpuChart.data.datasets[0].data[1] = 100 - formattedCpuLoad;
+
+    // Verificar si la carga de la CPU supera el umbral (por ejemplo, 50%)
+    if (cpuLoad > 50) {
+        // Agregar una animación al gráfico
+        cpuChart.options.animation = {
+            duration: 1000, // Duración de la animación en milisegundos
+            easing: 'easeInOutQuad' // Tipo de curva de la animación (opcional)
+        };
+    } else {
+        // Si la carga de la CPU está por debajo del umbral, eliminar la animación
+        cpuChart.options.animation = false;
+    }
+
+    // Actualizar el gráfico
+    cpuChart.update();
+}
+
+// Función para obtener y mostrar la carga de la CPU en el gráfico de donut
+function getCPULoadChartData() {
+    // Construir una URL con un parámetro de consulta único para evitar la caché del navegador
+    const timestamp = Date.now();
+    const apiUrl = `http://localhost:19999/api/v1/data?chart=system.cpu&after=-1&points=1&timestamp=${timestamp}`;
+
+    // Realizar la solicitud GET a la API de Netdata
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Obtener la carga de la CPU
+        const cpuLoad = parseFloat(data.data[0][6]);
+
+        // Actualizar el gráfico de donut
+        updateCPULoadChart(cpuLoad);
+    })
+    .catch(error => {
+        console.error('Error al obtener la carga de la CPU del sistema:', error);
+    });
+}
+
+// Llamar a la función para obtener la carga de la CPU inicialmente
+getCPULoadChartData();
+
+// Actualizar la carga de la CPU cada segundo
+setInterval(getCPULoadChartData, 1000);
+
+// Crear el gráfico de donut para la RAM
+var ramChart = new Chart(document.getElementById('ramChart').getContext('2d'), {
+    type: 'doughnut',
+    data: {
+        labels: ['Uso de RAM (MB)', 'Libre de RAM (MB)'],
+        datasets: [{
+            data: [0, 100],
+            backgroundColor: ['#ff6f61', '#f0f0f0'] // Cambiar los colores aquí
+        }]
+    },
+    options: {
+        aspectRatio: 1,
+        cutoutPercentage: 50,
+        plugins: {
+            legend: {
+                display: false
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        return 'Uso de RAM: ' + context.parsed + 'MB';
+                    }
+                }
+            }
+        }
+    }
+});
+
+// Función para actualizar el gráfico de donut de la RAM
+function updateRAMChart(usedRAM, freeRAM) {
+    // Formatear la carga de RAM para mostrar solo un decimal
+    const formattedUsedRAM = usedRAM.toFixed(1);
+    const formattedFreeRAM = freeRAM.toFixed(1);
+
+    // Actualizar el texto de la carga de RAM
+    const ramLoadText = formattedUsedRAM + ' MB';
+    document.querySelector('.ram-load').textContent = ramLoadText;
+
+    // Actualizar los datos del gráfico de donut de la RAM
+    ramChart.data.datasets[0].data[0] = formattedUsedRAM;
+    ramChart.data.datasets[0].data[1] = formattedFreeRAM;
+
+    // Actualizar el gráfico
+    ramChart.update();
+}
+
+// Función para obtener y mostrar la carga de RAM en el gráfico de donut
+function getRAMChartData() {
+    // Construir la URL de la API de Netdata para obtener datos de RAM
+    const timestamp = Date.now();
+    const apiUrl = `http://localhost:19999/api/v1/data?chart=system.ram&after=-1&points=1&timestamp=${timestamp}`;
+
+    // Realizar una solicitud GET a la API de Netdata
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Obtener los valores de RAM utilizada y libre
+        const usedRAM = parseFloat(data.data[0][2]);
+        const freeRAM = parseFloat(data.data[0][1]);
+
+        // Actualizar el gráfico de donut de la RAM
+        updateRAMChart(usedRAM, freeRAM);
+    })
+    .catch(error => {
+        console.error('Error al obtener la carga de RAM del sistema:', error);
+    });
+}
+
+// Llamar a la función para obtener la carga de RAM inicialmente
+getRAMChartData();
+
+// Actualizar la carga de RAM cada segundo
+setInterval(getRAMChartData, 1000);
+
+// Crear el gráfico de donut para Disco
+var diskChart = new Chart(document.getElementById('diskChart').getContext('2d'), {
+    type: 'doughnut',
+    data: {
+        labels: ['Uso de Disco (GB)', ''],
+        datasets: [{
+            data: [0, 100],
+            backgroundColor: ['#2ec4b6', '#f0f0f0'] // Cambiar los colores aquí
+        }]
+    },
+    options: {
+        aspectRatio: 1,
+        cutoutPercentage: 50,
+        plugins: {
+            legend: {
+                display: false
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        return 'Uso de Disco: ' + context.parsed + 'GB';
+                    }
+                }
+            }
+        }
+    }
+});
+
+// Función para obtener y mostrar el uso de Disco en el gráfico de donut
+function getDiskUsageData() {
+    // Construir la URL de la API de Netdata para obtener datos de Disco
+    const timestamp = Date.now();
+    const apiUrl = `http://localhost:19999/api/v1/data?chart=disk_space._&after=-1&points=1&timestamp=${timestamp}`;
+
+    // Realizar una solicitud GET a la API de Netdata
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Obtener los valores de Disco disponibles y utilizados
+        const availDisk = parseFloat(data.data[0][1]);
+        const usedDisk = parseFloat(data.data[0][2]);
+
+        // Calcular el total del Disco sumando los valores disponibles y utilizados
+        const totalDisk = Math.round((availDisk + usedDisk) * 10) / 10; // Redondear el resultado a 1 decimal
+
+        // Actualizar el gráfico de donut de Disco
+        updateDiskChart(usedDisk, totalDisk);
+    })
+    .catch(error => {
+        console.error('Error al obtener el uso de Disco del sistema:', error);
+    });
+}
+
+// Función para actualizar el gráfico de donut de Disco
+function updateDiskChart(usedDisk, totalDisk) {
+    const availDisk = totalDisk - usedDisk; // Calcular el Disco disponible restando el Disco utilizado del total
+
+    // Actualizar los datos del gráfico de donut de Disco
+    diskChart.data.datasets[0].data[0] = Math.round(usedDisk * 10) / 10; // Redondear el uso de Disco a 1 decimal
+    diskChart.data.datasets[0].data[1] = Math.round(availDisk * 10) / 10; // Redondear el Disco disponible a 1 decimal
+
+    // Actualizar el gráfico
+    diskChart.update();
+}
+
+// Llamar a la función para obtener el uso de Disco inicialmente
+getDiskUsageData();
+
+// Actualizar el uso de Disco cada segundo
+setInterval(getDiskUsageData, 1000);
+
+// Crear el gráfico de líneas para la lectura de disco
+var diskReadChart = new Chart(document.getElementById('diskReadChart').getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Lectura de Disco (MB/s)',
+            data: [],
+            borderColor: '#007bff',
+            backgroundColor: 'rgba(0, 123, 255, 0.1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                type: 'time',
+                time: {
+                    unit: 'second',
+                    tooltipFormat: 'HH:mm:ss',
+                    displayFormats: {
+                        second: 'HH:mm:ss'
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Tiempo'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Lectura de Disco (MB/s)'
+                }
+            }
+        }
+    }
+});
+
+// Función para agregar datos al gráfico de líneas de la lectura de disco
+function addDataToDiskReadChart(timestamp, diskReadUsageMB) {
+    // Agregar el punto de datos al conjunto de datos del gráfico
+    diskReadChart.data.labels.push(timestamp);
+    diskReadChart.data.datasets[0].data.push(diskReadUsageMB);
+
+    // Limitar la cantidad de puntos de datos mostrados en el gráfico para mantenerlo limpio
+    const maxDataPoints = 10;
+    if (diskReadChart.data.labels.length > maxDataPoints) {
+        diskReadChart.data.labels.shift();
+        diskReadChart.data.datasets[0].data.shift();
+    }
+
+    // Actualizar el gráfico
+    diskReadChart.update();
+}
+
+/// Función para obtener y mostrar la lectura de disco en el gráfico de líneas
+function getDiskReadChartData() {
+    const timestamp = new Date();
+    const apiUrl = `http://localhost:19999/api/v1/data?chart=disk.sda&after=-1&points=1&timestamp=${timestamp}`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Obtener los valores de lectura de disco
+        const diskReadUsageKB = parseFloat(data.data[0][1]);
+        const diskReadUsageMB = diskReadUsageKB / 1024;
+
+        // Actualizar el gráfico de líneas de la lectura de disco
+        addDataToDiskReadChart(timestamp, diskReadUsageMB);
+    })
+    .catch(error => {
+        console.error('Error al obtener la lectura de disco del sistema:', error);
+    });
+}
+
+// Llamar a la función inicialmente y luego establecer una actualización periódica
+getDiskReadChartData(); // Obtener datos inicialmente
+setInterval(getDiskReadChartData, 3000); // Actualizar cada 3 segundos
+
+// Crear el gráfico de líneas para la escritura de disco
+var diskWriteChart = new Chart(document.getElementById('diskWriteChart').getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Escritura de Disco (MB/s)',
+            data: [],
+            borderColor: '#007bff',
+            backgroundColor: 'rgba(0, 123, 255, 0.1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                type: 'time',
+                time: {
+                    unit: 'second',
+                    tooltipFormat: 'HH:mm:ss',
+                    displayFormats: {
+                        second: 'HH:mm:ss'
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Tiempo'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Escritura de Disco (MB/s)'
+                }
+            }
+        }
+    }
+});
+
+// Función para agregar datos al gráfico de líneas de la escritura de disco
+function addDataToDiskWriteChart(timestamp, diskWriteUsageMB) {
+    // Agregar el punto de datos al conjunto de datos del gráfico
+    diskWriteChart.data.labels.push(timestamp);
+    diskWriteChart.data.datasets[0].data.push(diskWriteUsageMB);
+
+    // Limitar la cantidad de puntos de datos mostrados en el gráfico para mantenerlo limpio
+    const maxDataPoints = 10;
+    if (diskWriteChart.data.labels.length > maxDataPoints) {
+        diskWriteChart.data.labels.shift();
+        diskWriteChart.data.datasets[0].data.shift();
+    }
+
+    // Actualizar el gráfico
+    diskWriteChart.update();
+}
+// Función para eliminar el signo negativo de los valores de escritura
+function eliminarSignoNegativo(valor) {
+    return Math.abs(valor);
+}
+
+// Ejemplo de uso
+let valorEscritura = -20.37715;
+let valorEscrituraPositivo = eliminarSignoNegativo(valorEscritura);
+console.log(valorEscrituraPositivo); // Salida: 20.37715
+
+// Función para obtener y mostrar la escritura de disco en el gráfico de líneas
+function getDiskWriteChartData() {
+    const timestamp = new Date();
+    const apiUrl = `http://localhost:19999/api/v1/data?chart=disk.sda&after=-1&points=1&timestamp=${timestamp}`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Obtener los valores de escritura de disco
+        let diskWriteUsageKB = parseFloat(data.data[0][2]);
+        
+        // Eliminar el signo negativo si lo hay
+        diskWriteUsageKB = eliminarSignoNegativo(diskWriteUsageKB);
+
+        // Convertir a megabytes
+        const diskWriteUsageMB = diskWriteUsageKB / 1024;
+
+        // Actualizar el gráfico de líneas de la escritura de disco
+        addDataToDiskWriteChart(timestamp, diskWriteUsageMB);
+    })
+    .catch(error => {
+        console.error('Error al obtener la escritura de disco del sistema:', error);
+    });
+}
+
+
+// Llamar a la función inicialmente y luego establecer una actualización periódica
+getDiskWriteChartData(); // Obtener datos inicialmente
+setInterval(getDiskWriteChartData, 3000); // Actualizar cada 3 segundos
+
+// Crear el gráfico de líneas para el uso de la red
+var networkChart = new Chart(document.getElementById('networkChart').getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Uso de la red (KB/s)',
+            data: [],
+            borderColor: '#007bff',
+            backgroundColor: 'rgba(0, 123, 255, 0.1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                type: 'time',
+                time: {
+                    unit: 'second',
+                    tooltipFormat: 'HH:mm:ss',
+                    displayFormats: {
+                        second: 'HH:mm:ss'
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Tiempo'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Uso de la red (KB/s)'
+                }
+            }
+        }
+    }
+});
+
+// Función para agregar datos al gráfico de líneas del uso de la red
+function addDataToNetworkChart(timestamp, networkUsageKB) {
+    // Agregar el punto de datos al conjunto de datos del gráfico
+    networkChart.data.labels.push(timestamp);
+    networkChart.data.datasets[0].data.push(networkUsageKB);
+
+    // Limitar la cantidad de puntos de datos mostrados en el gráfico para mantenerlo limpio
+    const maxDataPoints = 10;
+    if (networkChart.data.labels.length > maxDataPoints) {
+        networkChart.data.labels.shift();
+        networkChart.data.datasets[0].data.shift();
+    }
+
+    // Actualizar el gráfico
+    networkChart.update();
+}
+
+// Función para obtener y mostrar el uso de la red en el gráfico de líneas
+function getNetworkChartData() {
+    // Construir la URL de la API de Netdata para obtener datos de uso de la red
+    const timestamp = Date.now();
+    const apiUrl = `http://localhost:19999/api/v1/data?chart=system.net&after=-1&points=1&timestamp=${timestamp}`;
+
+    // Realizar una solicitud GET a la API de Netdata
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Obtener el valor del uso de la red
+        const networkUsageKB = parseFloat(data.data[0][1]) / 1024; // Convertir de B/s a KB/s
+
+        // Llamar a la función para agregar datos al gráfico de líneas del uso de la red
+        addDataToNetworkChart(timestamp, networkUsageKB);
+    })
+    .catch(error => {
+        console.error('Error al obtener el uso de la red del sistema:', error);
+    });
+}
+
+// Llamar a la función inicialmente y luego establecer una actualización periódica
+getNetworkChartData(); // Obtener datos inicialmente
+setInterval(getNetworkChartData, 3000); // Actualizar cada 3 segundos
